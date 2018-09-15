28d1ab76b74522cd9e246b857f40399148bdee88
Tomas Mūžas <muzas.tomas@gmail.com>
2018-09-15 19:40:28 +0300

    Add conversions to int and bool to Bit class

--- ConvolutionalCodes/Entities/Bit.cs
+++ ConvolutionalCodes/Entities/Bit.cs
@@ -23,6 +23,17 @@ namespace ConvolutionalCodes.Entities
         {
             return new Bit(value);
         }
+
+        public static explicit operator bool(Bit bit)
+        {
+            return bit._value;
+        }
+
+        public static explicit operator int(Bit bit)
+        {
+            return bit._value ? 1 : 0;
+        }
+
         public static bool operator ==(Bit bit1, Bit bit2)
         {
             return bit1._value == bit2._value;
