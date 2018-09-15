f0f39afd20f87653407ce3d8107f1253c3b80335
Tomas Mūžas <muzas.tomas@gmail.com>
2018-09-15 19:37:06 +0300

    Add Text Converter

---
+++ ConvolutionalCodes/Utilities/TextConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using ConvolutionalCodes.Entities;
+
+namespace ConvolutionalCodes.Utilities
+{
+    public class TextConverter : IConverter<string>
+    {
+        private Encoding _encoding { get; set; }
+
+        public TextConverter(Encoding encoding)
+        {
+            _encoding = encoding;
+        }
+
+        public string FromBitStream(IBitStream stream)
+        {
+            var bytes = stream.ToByteArray();
+            return _encoding.GetString(bytes);
+        }
+
+        public IBitStream ToBitStream(string value)
+        {
+            var bytes = _encoding.GetBytes(value);
+            return new BitStream(bytes);
+        }
+    }
+}
