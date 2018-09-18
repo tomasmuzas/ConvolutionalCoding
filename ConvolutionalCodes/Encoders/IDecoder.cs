5a3c8c8b2719a7c4f8af8a4df02c45882f823c6c
Tomas Mūžas <muzas.tomas@gmail.com>
2018-09-18 22:35:21 +0300

    Add Decoder interface

---
+++ ConvolutionalCodes/Encoders/IDecoder.cs
@@ -0,0 +1,11 @@
+﻿using ConvolutionalCodes.Entities;
+
+namespace ConvolutionalCodes.Encoders
+{
+    public interface IDecoder
+    {
+        IBitStream Decode(IBitStream stream);
+
+        void AddParityBitResolvers(ParityBitGenerator generator);
+    }
+}
