7bf38e2586060015c34762bc2c92b7d9f76951c1
Tomas Mūžas <muzas.tomas@gmail.com>
2018-09-22 22:36:22 +0300

    Add message controller

--- ConvolutionalCodes/Views/Application.cs
+++ ConvolutionalCodes/Views/Application.cs
@@ -1,4 +1,5 @@
-﻿using ConvolutionalCodes.Encoders;
+﻿using ConvolutionalCodes.Controllers;
+using ConvolutionalCodes.Encoders;
 using ConvolutionalCodes.Entities;
 using ConvolutionalCodes.Utilities;
 using System;
@@ -12,32 +13,22 @@ namespace ConvolutionalCodes
         public Application()
         {
             InitializeComponent();
-            
-            IConverter<string> converter = new TextConverter(Encoding.UTF8);
 
             var initialText = "Labas, mano vardas Tomas";
-            var initialBits = converter.ToBitStream(initialText);
 
             var channel = new NoisyChannel(errorChance: 0.2);
 
-            // Text without encoding
-            var bitsAfterTransmission = channel.Transmit(initialBits);
-            var unencodedTextAfterTransmission = converter.FromBitStream(bitsAfterTransmission);
+            var unencodedText = MessageController.SendText(initialText, channel);
 
-
-            // Text with encoding
-            var encoder = new ConvolutionalEncoder();
-            var encodedText = encoder.Encode(initialBits);
-
-            var transmittedEncodedText = channel.Transmit(encodedText);
-
-            var decoder = new ConvolutionalDecoder();
-            var decodedBitsAfterTransmission = decoder.Decode(transmittedEncodedText);
-            var encodedTextAfterTransmission = converter.FromBitStream(decodedBitsAfterTransmission);
+            var encodedText = MessageController.SendText(
+                initialText,
+                channel,
+                new ConvolutionalEncoder(),
+                new ConvolutionalDecoder());
 
             Console.WriteLine("Initial text:\t\t\t\t\t\t" + initialText);
-            Console.WriteLine("Unencoded text after transmission:\t\t\t" + unencodedTextAfterTransmission);
-            Console.WriteLine("Encoded text after transmission:\t\t\t" + encodedTextAfterTransmission);
+            Console.WriteLine("Unencoded text after transmission:\t\t\t" + unencodedText);
+            Console.WriteLine("Encoded text after transmission:\t\t\t" + encodedText);
 
         }
     }
