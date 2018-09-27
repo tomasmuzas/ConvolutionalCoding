a725fa10e2e8095812ad852fad5c31747f1ea3ff
Tomas Mūžas <muzas.tomas@gmail.com>
2018-09-27 21:03:01 +0300

    Add BitArray extensions

---
+++ ConvolutionalCodes/Extensions/BitArrayExtensions.cs
@@ -0,0 +1,16 @@
+﻿using ConvolutionalCodes.Entities;
+
+namespace ConvolutionalCodes.Extensions
+{
+    public static class BitArrayExtensions
+    {
+        public static Bit GetBitOrDefault(this Bit[] bits, int position)
+        {
+            if (position < bits.Length)
+            {
+                return bits[position];
+            }
+            return new Bit(false);
+        }
+    }
+}
