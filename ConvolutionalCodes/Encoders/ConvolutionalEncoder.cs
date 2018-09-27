ebd29021d01ea5f25981394e838ceb8def3ca3cf
Tomas Mūžas <muzas.tomas@gmail.com>
2018-09-27 21:04:32 +0300

    Refactor convolutional encoder to use Bit array and Bit Array Extensions

--- ConvolutionalCodes/Encoders/ConvolutionalEncoder.cs
+++ ConvolutionalCodes/Encoders/ConvolutionalEncoder.cs
@@ -1,58 +1,40 @@
-using System.Collections.Generic;
-using System.Linq;
+using System;
 using ConvolutionalCodes.Entities;
 using ConvolutionalCodes.Extensions;
 
 namespace ConvolutionalCodes.Encoders
 {
     public class ConvolutionalEncoder : IEncoder
-    {
-        private IRegister _register { get; set; }
-       
-
-        public ConvolutionalEncoder()
-        {
-            _register = new ShiftingRegister(6);
-        }
-
+    { 
         public IBitStream Encode(IBitStream stream)
         {
-            var encodedStream = new List<Bit>();
-            var bitsToEncode = stream
-                .ReadAllBits()
-                .Prepend(new [] {
-                    new Bit(0),
-                    new Bit(0),
-                    new Bit(0),
-                    new Bit(0),
-                    new Bit(0),
-                    new Bit(0)
-                })
-                .ToArray();
-            int position = 0;
+            var encodedStream = new Bit[2 * stream.Length + 12];
+
+            var bitsToEncode = new Bit[stream.Length + 6];
+            for (int i = 0; i < 6; i++)
+            {
+                bitsToEncode[i] = new Bit(false);
+            }
+
+            var bits = stream.ReadAllBits();
+            Array.Copy(bits, 0, bitsToEncode, 6, bits.Length);
+
+            var position = 0;
+            var streamPosition = 0;
             var bitsCount = bitsToEncode.Length;
 
             while (position < bitsCount)
             {
-                var inputBit = GetBitAt(bitsToEncode, position + 6);
+                var inputBit = bitsToEncode.GetBitOrDefault(position + 6);
                 var secondBit = inputBit
-                    ^ GetBitAt(bitsToEncode, position + 4) // reversed 2nd bit (6 - 2)
-                    ^ GetBitAt(bitsToEncode, position + 1) // reversed 5th bit (6 - 5)
-                    ^ GetBitAt(bitsToEncode, position); // reversed 6th bit (6 - 6)
-                encodedStream.Add(inputBit);
-                encodedStream.Add(secondBit);
+                    ^ bitsToEncode.GetBitOrDefault(position + 4) // reversed 2nd bit (6 - 2)
+                    ^ bitsToEncode.GetBitOrDefault(position + 1) // reversed 5th bit (6 - 5)
+                    ^ bitsToEncode.GetBitOrDefault(position); // reversed 6th bit (6 - 6)
+                encodedStream[streamPosition++] = inputBit;
+                encodedStream[streamPosition++] = secondBit;
                 position++;
             }
             return new BitStream(encodedStream);
-        }
-
-        private Bit GetBitAt(Bit[] bits, int position)
-        {
-            if (position < bits.Length)
-            {
-                return bits[position];
-            }
-            return new Bit(0);
         }
     }
 }
