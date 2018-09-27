c76b0dacafb1eed8ac649b10a58095afc02e7899
Tomas Mūžas <muzas.tomas@gmail.com>
2018-09-27 21:05:33 +0300

    Refactor BitStream to use arrays instead of lists

--- ConvolutionalCodes/Entities/BitStream.cs
+++ ConvolutionalCodes/Entities/BitStream.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,37 +7,31 @@ namespace ConvolutionalCodes.Entities
 {
     public class BitStream : IBitStream
     {
-        private IEnumerable<Bit> _data { get; set; }
+        private Bit[] _data { get; set; }
 
         private int _position { get; set; }
 
-        public int Length => _data.Count();
+        public int Length => _data.Length;
 
-        public BitStream()
-        {
-            _data = new List<Bit>();
-            _position = 0;
-        }
-
-        public BitStream(IEnumerable<Bit> bits)
+        public BitStream(Bit[] bits)
         {
             _data = bits;
             _position = 0;
         }
 
-        public BitStream(IEnumerable<byte> bytes)
+        public BitStream(byte[] bytes)
         {
-            var list = new List<Bit>();
-            foreach(var b in bytes)
-            {
+            _data = new Bit[bytes.Length * 8];
+            int position = 0;
+
+            foreach (var b in bytes)
+            { 
                 for (int i = 0; i <= 7; i++)
                 {
                     var bit = new Bit(b >> (7 - i));
-                    list.Add(bit); 
+                    _data[position++] = bit; 
                 }
             }
-
-            _data = list;
             _position = 0;
         }
 
@@ -68,36 +63,31 @@ namespace ConvolutionalCodes.Entities
             return returnData;
         }
 
-        public IEnumerable<Bit> ReadAllBits()
+        public Bit[] ReadAllBits()
         {
             return _data;
         }
 
         public byte[] ToByteArray()
         {
-//            var bytes = new List<byte>();
-//            int bitCount = 0;
-//            byte currentByte = 0;
-//
-//            foreach (var bit in _data)
-//            {
-//                // Shift 1 or 0 representation of a Bit to the right position
-//                var positionalBit = (int)bit << (7 - bitCount);
-//                // Set the right position of a byte
-//                currentByte = (byte)(currentByte  | positionalBit);
-//
-//                if (++bitCount == 8)
-//                {
-//                    bytes.Add(currentByte);
-//                    currentByte = 0;
-//                    bitCount = 0;
-//                }
-//            }
-//
-//            return bytes.ToArray();
-            var bitArray = new BitArray(_data.Select(b => (bool)b).ToArray());
-            byte[] bytes = new byte[bitArray.Length / 8];
-            bitArray.CopyTo(bytes, 0);
+            var bytes = new byte[_data.Length / 8];
+
+            int byteCount = 0;
+
+            for (int i = 0; i < _data.Length; i += 8)
+            {
+                byte currentByte = 0;
+                for (int j = 0; j <= 7; j++)
+                {
+                    var bit = _data[i + j];
+
+                    var positionalBit = (int)bit << (7 - j);
+                    currentByte = (byte)(currentByte | positionalBit);
+                }
+
+                bytes[byteCount++] = currentByte;
+            }
+
             return bytes;
         }
     }
